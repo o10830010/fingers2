174cf867c0887dfded6d38c9a5fc53518476108e
o10830010 <o1083001@gm.pu.edu.tw>
2021-04-19 15:16:23 +0800

    04/19

---
+++ app/src/main/java/com/example/fingers/Fingers.kt
@@ -0,0 +1,54 @@
+package com.example.fingers
+
+import android.content.Context
+import android.graphics.*
+import android.view.MotionEvent
+import android.view.View
+
+class Fingers(context: Context) : View(context) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    //var xPos:Float = 200f
+    //var yPos:Float = 200f
+
+    var Count:Int = 0;
+    var xPos = FloatArray(20)
+    var yPos = FloatArray(20)
+    var rainbow = IntArray(7)
+
+    lateinit var bitmap: Bitmap
+
+    init {
+        rainbow = context.getResources().getIntArray(R.array.rainbow)
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.robot)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawColor(Color.LTGRAY)
+
+        for (i in 0..Count-1) {
+            paint.color = rainbow[i % 7]
+            canvas.drawCircle(xPos[i], yPos[i], 80f, paint)
+        }
+        paint.color = Color.BLUE
+        paint.textSize = 50f
+        canvas.drawText("多指觸控，圓形呈現彩虹顏色！", 50f,200f, paint)
+
+
+        val SrcRect:Rect = Rect(0, 0, bitmap.width, bitmap.height) //裁切(顯示全部)
+        val DestRect:Rect = Rect(200, 300,
+                bitmap.width/4+200, bitmap.height/4+300) //原圖較大，縮成1/4顯示
+        canvas.drawBitmap(bitmap, SrcRect, DestRect, paint)
+    }
+    override fun onTouchEvent(event: MotionEvent): Boolean{
+        //xPos = event.getX()
+        //yPos = event.getY()
+        Count = event.getPointerCount()
+        for (i in 0..Count-1) {
+            xPos[i] = event.getX(i)
+            yPos[i] = event.getY(i)
+        }
+        invalidate()
+        return true
+    }
+}
